b9dd7e6e1693eeae7bf7a39849063728533e595c
ketrinciaa <felliciaa2721@gmail.com>
2026-02-13 10:12:48 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit as st
 
 st.title("_hitung _ is :blue[tabung] :rocket:")
-r = st.number_input(masukan jari-jari (cm)): ",0)
-t = st.number_input(masukan tinggi (cm): ",0)
+r = st.number_input("masukan jari-jari (cm)): ",0)
+t = st.number_input("masukan tinggi (cm): ",0)
