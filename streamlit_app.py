9ab9b662f1aaacc32570f6f11b719185f69eebcf
ketrinciaa <felliciaa2721@gmail.com>
2026-02-13 10:20:55 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,3 +3,8 @@ import streamlit as st
 st.title("_hitung _ is :blue[tabung] :rocket:")
 r = st.number_input("masukan jari-jari (cm)): ",0)
 t = st.number_input("masukan tinggi (cm): ",0)
+
+
+if st.button("hitung jari-jari", type="primary"):
+   v = math.pi*(r**2)*t
+   st.success(f'tinggi tabung adalah{v:2f}')
