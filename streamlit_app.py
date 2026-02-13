16961a7cf6cb656c771fab0280680e38d48f761c
ketrinciaa <felliciaa2721@gmail.com>
2026-02-13 10:28:08 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-import math
+import math, time 
 
 st.title("_hitung _ is :blue[tabung] :rocket:")
 r = st.number_input("masukan jari-jari (cm)): ",0)
@@ -7,5 +7,9 @@ t = st.number_input("masukan tinggi (cm): ",0)
 
 
 if st.button("hitung jari-jari", type="primary"):
+   loading = st.progress(0)
+   for i in range(100):
+    time.sleep(0.01)
+    loading progress(i+1)
    v = math.pi*(r**2)*t
    st.success(f'tinggi tabung adalah{v:2f}')
