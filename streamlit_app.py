2ddc557694c9aa3fccd373054cd71119399d9712
ketrinciaa <felliciaa2721@gmail.com>
2026-02-13 10:35:12 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,7 +10,7 @@ if st.button("hitung jari-jari", type="primary"):
    loading = st.progress(0)
    for i in range(100):
     time.sleep(0.01)
-    loading progress(i+1)
+    loading.progress(i+1)
       
    v = math.pi*(r**2)*t
    st.success(f'tinggi tabung adalah{v:2f}')
